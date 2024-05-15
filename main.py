1b09d1cc1634904b3d12d013ed93619ca801d19f
aayushaman4 <aayushaman4@gmail.com>
2024-05-15 19:21:45 +0530

    Call/Put

--- main.py
+++ main.py
@@ -70,6 +70,8 @@ async def fetch_data(
                             for row in csv_rows:
                                 ticker, expiration, strike, call_put, ms_of_day, open_interest, date = row
 
+                                call_put = 'Call' if 'C' else 'Put'
+
                                 strike_updated = int(strike)/1000
 
                                 date_y =date[:4]
